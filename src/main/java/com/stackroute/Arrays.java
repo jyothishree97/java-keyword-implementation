d10ba1202edf798df66de730294fde437169f8d8
jyothishree.a <jyothishree.a@cgi.com>
2019-08-03 10:45:47 +0530

    Examples of LoopCondition

---
+++ src/main/java/com/stackroute/Arrays.java
@@ -0,0 +1,24 @@
+package com.stackroute;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        //The Scanner class is used to get user input, and it is found in the java.util package.
+        //2-Dimensional array
+        int array[][] = new int[2][3];
+        System.out.println("enter the array");
+        for (int i = 0; i <= array.length; i++) {
+            for (int j = 0; j <= array[i].length; i++) {
+                System.out.println("enter values" + i);
+                array[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i <= array.length - 1; i++) {
+            for (int j = 0; j <= array[i].length - 1; j++)
+                System.out.println(array[i][j]);
+        }
+
+    }
+}
--- src/main/java/com/stackroute/LoopExample.java
+++ src/main/java/com/stackroute/LoopExample.java
@@ -1,7 +1,23 @@
 package com.stackroute;
 
+import java.util.Scanner;
+
 public class LoopExample {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        //The Scanner class is used to get user input, and it is found in the java.util package.
+        System.out.println("enter the num");
+        int num = scan.nextInt();
+        //for(Intialization;condition / testing statements; Increment Or Decrement)
+        for (int i = 0; i <= num; i++) {
+                System.out.print(i);
+        }
+
+        while (num!=0){
+            System.out.print(num);
+            //decrementing number by 1
+            num--;
+        }
     }
 }
+
